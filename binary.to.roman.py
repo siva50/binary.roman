19ac3775c82e78654c648e047b340d5e5aff4a5a
siva50 <80094666+siva50@users.noreply.github.com>
2022-07-26 10:36:19 +0530

    Add files via upload

---
+++ binary.to.roman.py
@@ -0,0 +1,20 @@
+a=int(input("enater any number"))
+if a==1:
+    print("I")
+elif a==2:
+    print("II")
+elif a==3:
+    print("III")
+elif a==4:
+    print("IV")
+elif a==5:
+    print("V")
+elif a==6:
+    print("VI")
+elif a==7:
+    print("VII")
+elif a==8:
+    print("VIII")
+else:
+    a==9
+    print("IX")
